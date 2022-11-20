b19a3badfffe04e2c9a79f410af7fb7b7da3ec0c
muskratman <balerka25@gmail.com>
2022-11-20 15:05:36 +0300

    //hiber branch

--- src/main/java/com/cookiebros/libmvc/util/BookValidator.java
+++ src/main/java/com/cookiebros/libmvc/util/BookValidator.java
@@ -1,2 +1,41 @@
-package com.cookiebros.libmvc.util;public class BookValidator {
+package com.cookiebros.libmvc.util;
+
+import com.cookiebros.libmvc.dao.BookDAO;
+import com.cookiebros.libmvc.models.Book;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+public class BookValidator implements Validator {
+
+    private final BookDAO bookDAO;
+
+    public BookValidator(BookDAO bookDAO) {
+        this.bookDAO = bookDAO;
+    }
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return false;
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+
+        Book book = (Book) target;
+
+        //title
+        if (book.getTitle().length() < 1 || book.getTitle().length() > 60)
+            errors.rejectValue("title", "", "Book's title should not be empty");
+
+        //author
+        if (book.getAuthor().length() < 1 || book.getAuthor().length() > 60)
+            errors.rejectValue("author", "", "Book's author should not be empty");
+
+        //yearOfPublishing
+        if (book.getYearOfPublishing() < 1600 || book.getYearOfPublishing() > 2022)
+            errors.rejectValue("yearOfPublishing", "", "yearOfPublishing should be between 1600 and 2022");
+
+    }
 }
--- src/main/java/com/cookiebros/libmvc/util/PersonValidator.java
+++ src/main/java/com/cookiebros/libmvc/util/PersonValidator.java
@@ -1,2 +1,49 @@
-package com.cookiebros.libmvc.util;public class PersonValidator {
+package com.cookiebros.libmvc.util;
+
+import com.cookiebros.libmvc.dao.PersonDAO;
+import com.cookiebros.libmvc.models.Person;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import java.util.regex.Pattern;
+
+@Component
+public class PersonValidator implements Validator {
+
+    private final PersonDAO personDAO;
+
+    public PersonValidator(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return false;
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+
+        Person person = (Person) target;
+
+        //FIO
+        int fioMin = 5;
+        int fioMax = 60;
+        String fioPattern = "[А-Я][а-я]{2,20} [А-Я][а-я]{2,20} [А-Я][а-я]{0,20}";
+
+        if (person.getFio().length() < fioMin || person.getFio().length() > fioMax) {
+            errors.rejectValue("fio", "", "FIO should be between 2 and 30 characters");
+        } else if (!Pattern.compile(fioPattern).matcher(person.getFio()).matches()) {
+            errors.rejectValue("fio", "", "Invalid characters in FIO");
+            //проверка на уникальность FIO в БД
+        } else if(personDAO.show(person.getFio()).isPresent()) {
+            errors.rejectValue("fio", "", "This FIO is already taken");
+        }
+
+        //YearOfBirth
+        if (person.getYearOfBirth() < 1900 || person.getYearOfBirth() > 2022)
+            errors.rejectValue("yearOfBirth", "", "yearOfBirth should be greater then 1900");
+    }
 }
