a190d98456db60b70f6bd760d499d2516e2de253
muskratman <balerka25@gmail.com>
2022-11-20 20:48:19 +0300

    //hiber3

--- src/main/java/com/cookiebros/libmvc/controllers/BooksController.java
+++ src/main/java/com/cookiebros/libmvc/controllers/BooksController.java
@@ -1,3 +1,99 @@
 package com.cookiebros.libmvc.controllers;
+
+import com.cookiebros.libmvc.dao.BookDAO;
+import com.cookiebros.libmvc.dao.PersonDAO;
+import com.cookiebros.libmvc.models.Book;
+import com.cookiebros.libmvc.models.Person;
+import com.cookiebros.libmvc.util.BookValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/books")
 public class BooksController {
+    private final BookDAO bookDAO;
+    private final BookValidator bookValidator;
+    private PersonDAO personDAO;
+
+    @Autowired
+    public BooksController(BookDAO bookDAO, BookValidator bookValidator, PersonDAO personDAO) {
+        this.bookDAO = bookDAO;
+        this.bookValidator = bookValidator;
+        this.personDAO = personDAO;
+    }
+
+
+
+    @GetMapping()
+    public String index(Model model){
+        model.addAttribute("books", bookDAO.index());
+        return "books/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model, @ModelAttribute("person") Person person) {
+        Book thisBook = bookDAO.show(id);
+        model.addAttribute("book", thisBook);
+        model.addAttribute("owner", personDAO.show(thisBook.getPersonId()));
+        model.addAttribute("people", personDAO.index());
+        return "books/show";
+    }
+
+    @GetMapping("/new")
+    public String newBook(@ModelAttribute("book") Book book) {
+        return "books/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("book") Book book, BindingResult bindingResult) {
+        bookValidator.validate(book, bindingResult);
+
+        if (bindingResult.hasErrors())
+            return "books/new";
+        bookDAO.save(book);
+        return "redirect:/books";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(@PathVariable("id") int id, Model model) {
+        model.addAttribute("book", bookDAO.show(id));
+        return "books/edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@PathVariable("id") int id, @ModelAttribute("book") Book book,
+                         BindingResult bindingResult) {
+        bookValidator.validate(book, bindingResult);
+
+        if (bindingResult.hasErrors())
+            return "books/edit";
+
+        bookDAO.update(id, book);
+        return "redirect:/books";
+    }
+
+    @PostMapping("/{id}/add_owner")
+    public String addOwner(@PathVariable("id") int id,
+                           @ModelAttribute("person") Person person) {
+        bookDAO.addOwner(id, person.getId());
+        return "redirect:/books/" + id;
+    }
+
+    @PostMapping("/{id}/release")
+    public String releaseBook(@PathVariable("id") int id) {
+        bookDAO.addOwner(id, 1);
+        return "redirect:/books/" + id;
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id) {
+        bookDAO.delete(id);
+        return "redirect:/books";
+    }
 }
