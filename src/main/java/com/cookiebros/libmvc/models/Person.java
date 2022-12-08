51cfe405ea07f450d65ba13661c40794d26d9bd7
muskratman <balerka25@gmail.com>
2022-12-09 00:34:19 +0300

    //Boot

--- src/main/java/com/cookiebros/libmvc/models/Person.java
+++ src/main/java/com/cookiebros/libmvc/models/Person.java
@@ -1,6 +1,7 @@
 package com.cookiebros.libmvc.models;
 
-import jakarta.persistence.*;
+//import jakarta.persistence.*;
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
