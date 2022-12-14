c0f7589dfd299a894a4a7387dc7dbcfe75fbc88e
muskratman <balerka25@gmail.com>
2022-12-14 18:30:54 +0300

    // security branch

--- src/main/java/com/cookiebros/libmvc/models/Person.java
+++ src/main/java/com/cookiebros/libmvc/models/Person.java
@@ -27,11 +27,26 @@ public class Person {
     @Column(name = "year_of_birth")
     private int yearOfBirth;
 
-    @Column(name = "email")
-    private String email;
-
-    @Column(name = "password")
-    private String password;
+//    @Column(name = "email")
+//    private String email;
+//    @Column(name = "password")
+//    private String password;
+//
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
 
     @OneToMany(mappedBy = "owner")
     private List<Book> books;
